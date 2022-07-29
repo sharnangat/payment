59e99360d56b00a17e38fa924964c0cd59ea8b62
sharnangat <sanjaysharnangat@gmail.com>
2022-07-29 13:53:57 +0530

    add
    added

---
+++ src/test/java/com/payment/system/Account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.system.Account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
