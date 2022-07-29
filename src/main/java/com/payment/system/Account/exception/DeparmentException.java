2f6e519cc72a4a654fa20fff58e9b9eac79be5d7
sharnangat <sanjaysharnangat@gmail.com>
2022-07-29 18:33:10 +0530

    Code added for Department
    Code added for department

--- src/main/java/com/payment/system/Account/exception/DeparmentException.java
+++ src/main/java/com/payment/system/Account/exception/DeparmentException.java
@@ -2,4 +2,29 @@ package com.payment.system.Account.exception;
 
 public class DeparmentException extends Exception {
 
-}
+	public DeparmentException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeparmentException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeparmentException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeparmentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeparmentException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	}
